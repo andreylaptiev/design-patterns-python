655cf82570acd61e632fc0e613216427dfd08912
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-10-08 20:50:49 +0300

    Overrided __copy__ magick method. Copy of object created correctly

--- creational/prototype.py
+++ creational/prototype.py
@@ -1,8 +1,18 @@
-from abc import ABC, abstractmethod
+import copy
 
 
-# interface
-class Prototype(ABC):
-    @abstractmethod
-    def clone():
-        pass
+class Auto:
+    def __init__(self, wheels_quantity, engine):
+        self.wheels_quantity = wheels_quantity
+        self.engine = engine
+
+    def __copy__(self):
+        new = self.__class__(self.wheels_quantity, self.engine)
+        new.__dict__.update(self.__dict__)
+        return new
+
+
+if __name__ == '__main__':
+    auto = Auto(4, True)
+    auto_copy = copy.copy(auto)
+    print(auto_copy)
