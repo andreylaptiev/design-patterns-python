de410c0a6d0f7adbc4208c557dae142b7398d109
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-10-10 19:47:32 +0300

    Final version

--- creational/singleton.py
+++ creational/singleton.py
@@ -9,5 +9,12 @@ class SingletonMeta(type):
 
 
 class Singleton(metaclass=SingletonMeta):
-    def business_logic(self):
-        pass
+    def create_new_instance(self):
+        return Singleton()
+
+
+if __name__ == '__main__':
+    s = Singleton()
+    s1 = s.create_new_instance()
+    print(s == s1)
+    print(s is s1)
