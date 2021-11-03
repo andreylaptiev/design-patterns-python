d3771e43312b0e2eccaa48c89a6bd613fccb108a
Andrey Laptiev <andreylaptiev13@gmail.com>
2021-11-03 19:25:11 +0200

    Refactored. Final version

--- behavioral/iterator.py
+++ behavioral/iterator.py
@@ -1,59 +1,119 @@
-from collections.abc import Iterator, Iterable
+from abc import ABC, abstractmethod
+from collections.abc import Iterable, Iterator
+from random import choice, randint
 
 
-# concrete iterator
+class Earth:
+    def __init__(self):
+        self.__name = 'earth'
+        self.__cell_quantity = 1
+
+    @property
+    def cell_quantity(self):
+        return self.__cell_quantity
+
+    @property
+    def name(self):
+        return self.__name
+
+
+class Lava:
+    def __init__(self):
+        self.__name = 'lava'
+        self.__cell_quantity = 3
+
+    @property
+    def cell_quantity(self):
+        return self.__cell_quantity
+
+    @property
+    def name(self):
+        return self.__name
+
+
+class Factory(ABC):
+    @staticmethod
+    @abstractmethod
+    def create_element():
+        pass
+
+
+class EarthFactory(Factory):
+    @staticmethod
+    def create_element():
+        return Earth()
+
+
+class LavaFactory(Factory):
+    @staticmethod
+    def create_element():
+        return Lava()
+
+
 class ConcreteIterator(Iterator):
     def __init__(self, collection):
-        self._collection = collection
-        self._index = 0
+        self.__collection = collection
+        self.__index = 0
 
     def __next__(self):
-        boys_students = []
-        girls_students = []
-
         try:
-            for _ in self._collection:
-                student = self._collection[self._index]
-                self._index += 1
-
-                for k, v in student.items():
-                    if v == 'Boy':
-                        boys_students.append(student)
-                    else:
-                        girls_students.append(student)
+            for _ in self.__collection:
+                element = self.__collection[self.__index]
+                if isinstance(element, Earth):
+                    print(f'{element.name}')
+                    self.__index += 1
+                elif isinstance(element, Lava):
+                    self.__index += element.cell_quantity
         except IndexError:
-            raise StopIteration()
-
-        return boys_students, girls_students
+            StopIteration()
 
 
-# concrete iterable
 class ConcreteIterable(Iterable):
-    def __init__(self, collection,):
-        self._collection = collection
+    def __init__(self, earth_factory: Factory, lava_factory: Factory):
+        self._earth_factory = earth_factory
+        self._lava_factory = lava_factory
+        self.__collection = self.__create_iterable()
+
+    def __create_iterable(self):
+        collection = []
+        earth_element = self._earth_factory.create_element()
+        lava_element = self._lava_factory.create_element()
+        possible_element = (earth_element, lava_element)
+
+        for _ in range(randint(10, 15)):
+            element = choice(possible_element)
+            if isinstance(element, Earth):
+                for _ in range(element.cell_quantity):
+                    collection.append(element)
+            else:
+                for _ in range(element.cell_quantity):
+                    collection.append(element)
+
+        return collection
+
+    @property
+    def collection(self):
+        return self.__collection
 
     def __iter__(self):
-        return ConcreteIterator(self._collection)
+        return ConcreteIterator(self.__collection)
 
-    def get_students(self):
-        return self.__iter__().__next__()
-
-
-def client_code():
-    collection = [{'John': 'Boy'}, {'Ann': 'Girl'}, {'Kate': 'Girl'}, {'Tom': 'Boy'}]
-
-    iterable = ConcreteIterable(collection)
-    iteration = iterable.get_students()
-
-    print('Iteration result: boys students')
-    print(iteration[0], end='\n\n')
-
-    print('Iteration result: girls students')
-    print(iteration[1])
+    def iterate(self):
+        self.__iter__().__next__()
 
 
 def main():
-    client_code()
+    earth_factory = EarthFactory()
+    lava_factory = LavaFactory()
+
+    collection = ConcreteIterable(earth_factory, lava_factory)
+
+    for element in collection.collection:
+        print(f'{element.name}', end=' ')
+
+    print('')
+
+    collection.iterate()
 
 
 if __name__ == '__main__':
